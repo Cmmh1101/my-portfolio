1f22c8b3aff33c2d9115e890c10acfb6a01e2464
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-03-06 07:41:33 -0600

    deleted unused files

--- public/app.js
+++ public/app.js
@@ -3,6 +3,11 @@ var menuBtn = document.getElementsByClassName('menu-btn')
 
 var mobileMenu = document.getElementsByClassName('nav-menu')
 
+// remove nav-menu when X is clicked
+
+var closeBtn = document.getElementsByClassName('close')
+
+
 var clickedBtn = function() {
     mobileMenu[0].classList.toggle('active')
 }
@@ -11,7 +16,32 @@ menuBtn[0].addEventListener('click', clickedBtn)
 
 console.log(menuBtn[0])
 
+// remove nav-menu when X is clicked
+
+var clickedBtn = function() {
+    mobileMenu[0].classList.toggle('hide')
+}
+
+menuBtn[0].addEventListener('click', clickedBtn)
+
+console.log(closeBtn[0])
+
+
+
 // End of Responsive Menu
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName('nav-menu').style.top = "0";
+  } else {
+    document.getElementsByClassName('nav-menu').style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 // ================ Menu scoll down hides, scroll up appears
 
