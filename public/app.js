3a35c97afbf531d70b3f38aedd5f5758fe3c91a5
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-03-15 19:18:18 -0500

    Added testimonials section, contact me and footer section

--- public/app.js
+++ public/app.js
@@ -36,19 +36,36 @@ closeMenu[0].addEventListener('click', clickedX)
 
 // End of Responsive Menu
 
-// ================ tooltip
+// slideshow
 
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip()
-//   });
+var slideIndex = 1;
+showSlides(slideIndex);
 
-//   $(document).ready(function(){
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// $(document).ready(function(){
-//   $('[data-toggle="tooltip"]').tooltip();
-// });
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 // ================ Menu scoll down hides, scroll up appears
 
